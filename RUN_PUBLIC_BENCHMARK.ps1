ac5ba5dfef522ee482d27fb48b3f771813d84cea
caizhongqi <49096944+caizhongqi@users.noreply.github.com>
2026-06-20 17:50:49 +0800

    build: add public benchmark helper

---
+++ RUN_PUBLIC_BENCHMARK.ps1
@@ -0,0 +1,6 @@
+& "D:\ProgramData\py2\python.exe" run_public_datasets_benchmark.py `
+  --datasets MNIST FashionMNIST FordA Wafer ElectricDevices `
+  --query-budget 1024 `
+  --qnn-epochs 18 `
+  --image-epochs 3 `
+  --ts-epochs 6
